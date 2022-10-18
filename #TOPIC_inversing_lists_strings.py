ec7c42d2f6444c0a7f7b24186f6b2f12c0938ef4
Hasan Saleh <71003887+hsnsl@users.noreply.github.com>
2022-10-18 13:07:35 +0300

    Update #TOPIC_inversing_lists_strings.py

--- #TOPIC_inversing_lists_strings.py
+++ #TOPIC_inversing_lists_strings.py
@@ -2,4 +2,5 @@
 #string
 print(input()[::-1]) 
 #list
-print(list(('ahmed','basil','mohamed'))[::-1])
+print(list(('ahmed','basil','mohamed'))[::-1]) ######without static change on the list 
+list(('ahmed','basil','mohamed').reverse() #####static change on the list
